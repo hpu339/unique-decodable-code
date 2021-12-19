00b5cda82f6267548ef2b32bd3d014b25b86c25d
Ylx <55937722+hpu339@users.noreply.github.com>
2021-12-19 20:41:54 +0800

    Update 尾随后缀法.py

--- 尾随后缀法.py
+++ 尾随后缀法.py
@@ -4,7 +4,7 @@
 # LastEditTime: 2021-12-19 19:27:47
 # LastEditors: YLX
 # Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-# FilePath: \.vscode\python\哈夫曼编码\hafum.py
+# FilePath: \.vscode\python\尾随后缀法\尾随后缀法.py
 # '''
 def not_empty(s):
     return s and s.strip()
